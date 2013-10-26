ca7706c130f287b08cb667357a433d8a3a63134c
Luis A. Echeverria <pixelsnob@gmail.com>
2013-10-25 23:17:23 -0700

    Cleanup

--- public/js/modules/socket.js
+++
@@ -1,8 +0,0 @@
-
-define([ 'socketio' ], function(socketio) {
-  var socket = socketio.connect(window.document.domain, {});
-  socket.on('error', function() {
-    console.log('socketio error');
-  });
-  return socket;
-});
