267694149763bc61996cb89c782c40e01456b822
Luis A. Echeverria <pixelsnob@gmail.com>
2013-12-01 23:52:18 -0800

    Added a modal base view

--- public/js/views/cms/meta.js
+++ public/js/views/cms/meta.js
@@ -4,46 +4,18 @@
  */
 define([
   'backbone',
-  'forms/cms/meta',
-  'jade',
-  'modules/events',
-  'bootstrap'
-], function(Backbone, MetaForm, jade, events) {
-  return Backbone.View.extend({
-    events: {
-      'click .btn-primary':  'save'
-    },
-    template: this.$(jade.render('modal')),
+  'views/modal',
+  'forms/cms/meta'
+], function(Backbone, ModalView, MetaForm) {
+  return ModalView.extend({
     initialize: function() {
-      this.setElement(this.template);
       this.form = new MetaForm({
         model: this.model,
         fields: [ 'title', 'keywords', 'description' ]
       });
       this.$el.find('.modal-title').text('Edit meta information');
       this.$el.find('.modal-body').html(this.form.render().el);
-      // Make sure form stays updated, since we only render it once
-      this.listenTo(this.model, 'change', function(model) {
-        _.each(this.form.fields, _.bind(function(field, name) {
-          var obj = {};
-          obj[name] = this.model.get(name);
-          this.form.setValue(obj);
-        }, this));
-      });
-    },
-    save: function(ev) {
-      var errors = this.form.commit();
-      if (typeof errors == 'undefined') {
-        this.$el.modal('hide');
-      }
-      return false;
-    },
-    render: function() {
-      return this.el;
-    },
-    modal: function() {
-      this.render();
-      this.$el.modal();
+      ModalView.prototype.initialize.apply(this);
     }
   });
 });
---
+++ public/js/views/modal.js
@@ -0,0 +1,41 @@
+/**
+ * Modal base view
+ * 
+ */
+define([
+  'backbone',
+  'jade',
+  'bootstrap'
+], function(Backbone, jade) {
+  return Backbone.View.extend({
+    events: {
+      'click .btn-primary':  'save'
+    },
+    modal_opts: { backdrop: 'static', keyboard: true },
+    template: this.$(jade.render('modal')),
+    initialize: function() {
+      this.setElement(this.template);
+      // Make sure form stays updated, since we only render it once
+      this.listenTo(this.model, 'change', function(model) {
+        _.each(this.form.fields, _.bind(function(field, name) {
+          var obj = {};
+          obj[name] = this.model.get(name);
+          this.form.setValue(obj);
+        }, this));
+      });
+    },
+    save: function(ev) {
+      var errors = this.form.commit();
+      if (typeof errors == 'undefined') {
+        this.$el.modal('hide');
+      }
+      return false;
+    },
+    render: function() {
+      return this.el;
+    },
+    modal: function() {
+      this.$el.modal(this.modal_opts);
+    }
+  });
+});
